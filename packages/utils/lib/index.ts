567d20c10bf5381e3e07dd2376ddf2563c91a9b5
志佳 <495725428@qq.com>
2022-02-12 16:46:12 +0800

    Update index.ts

--- packages/utils/lib/index.ts
+++ packages/utils/lib/index.ts
@@ -2,4 +2,8 @@
 import core from '@yideng/core';
 console.log('core: ', core);
 
-console.log('core: ', core);
+const normalTask = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('normal task');
+  }, 1000);
+});
